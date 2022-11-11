beb1d29a3ca61d3ca200b6311bdf666ab93a6d70
SanjaySrikar <49580566+SanjaySrikar@users.noreply.github.com>
2022-11-11 06:07:05 +0530

    web app design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 const routes: Routes = [
-  {path : 'landing' , component:LandingPageComponent}
+  {path : 'landing' , component:LandingPageComponent},
+  {path : '' , redirectTo : '/landing', pathMatch:'full'}
 ];
 
 @NgModule({
